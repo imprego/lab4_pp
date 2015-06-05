4f642be2384889ff04704fc03c1bd0bf86c8d98f
imprego <ckeapp@hotmail.com>
2015-06-05 12:27:58 +0300

    Application!!!!!!!!!!

--- src/com/miet/lab4_pp/Smoking.java
+++ src/com/miet/lab4_pp/Smoking.java
@@ -4,28 +4,28 @@ class Triple{
     double x,y,z;
 }
 public class Smoking {
-    public Triple add(double x,double y, double z, double x1, double y1, double z1){
+    public  Triple add(double x,double y, double z, double x1, double y1, double z1){
         Triple temp = new Triple();
         temp.x = x + x1;
         temp.y = y + y1;
         temp.z = z + z1;
         return temp;
     }
-    public Triple sub(double x,double y, double z, double x1, double y1, double z1){
+    public  Triple sub(double x,double y, double z, double x1, double y1, double z1){
         Triple temp = new Triple();
         temp.x = x - x1;
         temp.y = y - y1;
         temp.z = z - z1;
         return temp;
     }
-    public Triple scalarMul(double x,double y, double z, double x1, double y1, double z1){
+    public  Triple scalarMul(double x,double y, double z, double x1, double y1, double z1){
         Triple temp = new Triple();
         temp.x = x * x1;
         temp.y = y * y1;
         temp.z = z * z1;
         return temp;
     }
-    public Triple vectorMul(double x,double y, double z, double x1, double y1, double z1){
+    public  Triple vectorMul(double x,double y, double z, double x1, double y1, double z1){
         Triple temp = new Triple();
         temp.x = y * z1 - z * y1 ;
         temp.y = z * x1 - x * z1;
